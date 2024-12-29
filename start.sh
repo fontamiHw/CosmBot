4ddfcbd5f5447e826b8f28b919c28fa31673ce62
MikHw <fontami1@gmail.com>
2024-12-29 14:37:42 +0100

    create requested directory

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@ export APPLOGS="./host/logs"
 export RESOURCE_PATH="./host/resources"
 export APP_PR_FILES="./host/pr"
 
-rm -rf ${APPLOGS}/*
+mkdir -p ${APPLOGS}
+mkdir -p ${RESOURCE_PATH}
 mkdir -p ${APP_PR_FILES}
+rm -rf ${APPLOGS}/*
+rm -rf ${RESOURCE_PATH}/*
 rm -rf ${APP_PR_FILES}/*
 
 cp CosmWebex-debug-config.yaml ${RESOURCE_PATH}/CosmWebex-config.yaml
