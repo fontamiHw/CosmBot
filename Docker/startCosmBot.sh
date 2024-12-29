f2df768501c73ab147f744f3317785c388743f64
MikHw <fontami1@gmail.com>
2024-12-29 15:15:00 +0100

    tested in docker

--- Docker/startCosmBot.sh
+++ Docker/startCosmBot.sh
@@ -7,5 +7,5 @@ yaml_file="${RESOURCE_PATH}/CosmWebex-config.yaml"
 # Use yq to extract the value of 'port'
 CONTAINERCOMM_PORT=$(yq eval '.container_communication.port' "$yaml_file")
 
-echo "docker run --network Cosm-net --ip 10.58.1.11 -p ${CONTAINERCOMM_PORT}:${CONTAINERCOMM_PORT} -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot"
-docker run --network Cosm-net --ip 10.58.1.11 -p ${CONTAINERCOMM_PORT}:${CONTAINERCOMM_PORT} -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot
+echo "docker run --network Cosm-net --ip 10.58.1.11 -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot"
+docker run --network Cosm-net --ip 10.58.1.11  -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot
