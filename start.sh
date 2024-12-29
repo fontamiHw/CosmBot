b8d749de6247664a73c7d2312ed8010786b2ba90
MikHw <fontami1@gmail.com>
2024-12-29 09:35:47 +0100

    the restApi call /pr works

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 export APPLOGS="./host/logs"
 export RESOURCE_PATH="./host/resources"
 
-cp CosmWebex-config.yaml ${RESOURCE_PATH}/CosmWebex-config.yaml
+rm -rf ${APPLOGS}/*
+
+cp CosmWebex-debug-config.yaml ${RESOURCE_PATH}/CosmWebex-config.yaml
 python3 src/main.py
