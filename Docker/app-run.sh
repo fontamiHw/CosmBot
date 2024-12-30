67aa787228fe40c8ee7496ee5af3dac838345691
MikHw <fontami1@gmail.com>
2024-12-30 09:31:17 +0100

    Start using supervisord

---
+++ Docker/app-run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+########### PREPARE MOUNTED VOLUME ###########
+echo "Creating mounted directories"
+mkdir -p ${APPLOGS} 
+mkdir -p ${RESOURCE_PATH}
+mkdir -p ${APP_PR_FILES}
+
+########### PREPARE CONFIG IN MOUNTED VOLUME ###########
+echo "move resource files int the mounted path"
+filename="CosmWebex-config.yaml"
+# Variables for source and destination directories
+source_file=${TMP_RESOURCE_PATH}/${filename}
+destination_dir=${RESOURCE_PATH} 
+
+# Check if the file already exists in the destination directory
+if [ ! -e "$destination_dir/$filename" ]; then
+    # Copy the file to the destination directory
+    cp "$source_file" "$destination_dir/"
+    echo "File '$filename' copied to '$destination_dir'."
+else
+    echo "File '$filename' already exists in '$destination_dir'. No action taken."
+fi
+echo
+echo "show mounted volume"
+ls -latR /app/host
+echo
+
+########### ENVIROMENT ###########
+export PATH="$APP_COSM_PATH:$PATH"
+echo "show enviroment variable"
+env
+echo
+
+########### START APPLICATION ###########
+echo "moving in ${APP_COSM_PATH}"
+cd ${APP_COSM_PATH}
+pwd
+echo "Cosm Webex Starting....."
+python3 main.py
+#python3 clientTest.py
--- Docker/startCosmBot.sh
+++ Docker/startCosmBot.sh
@@ -3,13 +3,19 @@ HOST="../host"
 export APPLOGS="${HOST}/logs"
 export RESOURCE_PATH="${HOST}/resources"
 export APP_PR_FILES="${HOST}/pr"
+export SUPERVISOR_LOGS="${APPLOGS}/supervisord"
+
+echo ${SUPERVISOR_LOGS}
 
 mkdir -p ${APPLOGS}
 mkdir -p ${RESOURCE_PATH}
 mkdir -p ${APP_PR_FILES}
-rm -rf ${APPLOGS}/*
-rm -rf ${RESOURCE_PATH}/*
+mkdir -p ${SUPERVISOR_LOGS}
+ls -laR ${HOST}
+rm -rf ${APPLOGS}/*.log
+rm -rf ${RESOURCE_PATH}/*.yaml
 rm -rf ${APP_PR_FILES}/*
+rm -rf ${SUPERVISOR_LOGS}/*.log
 cp ../CosmWebex-config.yaml ${RESOURCE_PATH}/CosmWebex-config.yaml
 
 
@@ -20,4 +26,4 @@ yaml_file="${RESOURCE_PATH}/CosmWebex-config.yaml"
 CONTAINERCOMM_PORT=$(yq eval '.container_communication.port' "$yaml_file")
 
 echo "docker run -d --network Cosm-net --ip 10.58.1.11 -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot"
-docker run -d --network Cosm-net --ip 10.58.1.11  -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot
+docker run --network Cosm-net --ip 10.58.1.11  -v /home/civico129/MyProject/COSM-webex/host:/app/host --name CosmBot cosm-bot
